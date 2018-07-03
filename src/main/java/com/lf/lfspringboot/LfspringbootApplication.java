efa0fbc9147d009e23d8bfd3d24e4a39983f2da0
liufei <liufei@cnbmtech.com>
2018-07-03 10:09:48 +0800

    lf:1

---
+++ src/main/java/com/lf/lfspringboot/LfspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.lf.lfspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfspringbootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lf/lfspringboot/LfSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lf.lfspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LfSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
